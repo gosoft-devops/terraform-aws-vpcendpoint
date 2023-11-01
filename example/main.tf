f032e2b2c044e378cae579cc65f8224f3b23a8a6
Thanakrit Niljantuek <45716791+thanakrit1997@users.noreply.github.com>
2023-11-01 13:48:15 +0700

    update example.

---
+++ example/main.tf
@@ -0,0 +1,59 @@
+locals {
+  tags = {
+    Service     = ""
+    System      = ""
+    Owner       = ""
+    Project     = ""
+    Environment = ""
+    Manageby    = "terraform"
+    Createby    = ""
+  }
+}
+
+module "vpc_endpoint" {
+  source = "github.com/gosoft-devops/terraform-aws-vpcendpoint?ref=v1.0.0"
+
+  vpc_id = ""
+  vpc_endpoint = {
+    s3 = {
+      service      = "s3"
+      service_type = "Gateway"
+    }
+    dynamodb = {
+      service      = "dynamodb"
+      service_type = "Gateway"
+    }
+    api_gateway = {
+      service             = "execute-api"
+      service_type        = "Interface"
+      private_dns_enabled = false
+    }
+    ecrapi = {
+      service             = "ecr.api"
+      service_type        = "Interface"
+      private_dns_enabled = true
+    }
+    ecrdkr = {
+      service             = "ecr.dkr"
+      service_type        = "Interface"
+      private_dns_enabled = true
+    }
+    cloudwatchlogs = {
+      service             = "logs"
+      service_type        = "Interface"
+      private_dns_enabled = false
+    }
+    sqs = {
+      service             = "sqs"
+      service_type        = "Interface"
+      private_dns_enabled = false
+    }
+    sns = {
+      service             = "sns"
+      service_type        = "Interface"
+      private_dns_enabled = false
+    }
+  }
+
+  tags = local.tags
+}
