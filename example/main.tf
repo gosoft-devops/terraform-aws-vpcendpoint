0b702b66da0c1b41a30d4132da948ad074210f28
Thanakrit Niljantuek <45716791+thanakrit1997@users.noreply.github.com>
2023-11-01 14:13:01 +0700

    update example

--- example/main.tf
+++ example/main.tf
@@ -1,5 +1,15 @@
 locals {
-  tags = {
+  tags_01 = {
+    Service     = ""
+    System      = ""
+    Owner       = ""
+    Project     = ""
+    Environment = ""
+    Manageby    = "terraform"
+    Createby    = ""
+  }
+
+  tags_02 = {
     Service     = ""
     System      = ""
     Owner       = ""
@@ -10,7 +20,7 @@ locals {
   }
 }
 
-module "vpc_endpoint" {
+module "vpc_endpoint_vpc_000" {
   source = "github.com/gosoft-devops/terraform-aws-vpcendpoint?ref=v1.0.0"
 
   vpc_id = ""
@@ -57,5 +67,54 @@ module "vpc_endpoint" {
     }
   }
 
-  tags = local.tags
+  tags = local.tags_01
+}
+module "vpc_endpoint_vpc_111" {
+  source = "github.com/gosoft-devops/terraform-aws-vpcendpoint?ref=v1.0.0"
+
+  vpc_id = ""
+  # enable_security_group = false  ## False when custom Security Group.
+  # security_group_ids = [""]
+  vpc_endpoint = {
+    s3 = {
+      service      = "s3"
+      service_type = "Gateway"
+    }
+    dynamodb = {
+      service      = "dynamodb"
+      service_type = "Gateway"
+    }
+    api_gateway = {
+      service             = "execute-api"
+      service_type        = "Interface"
+      private_dns_enabled = false
+    }
+    ecrapi = {
+      service             = "ecr.api"
+      service_type        = "Interface"
+      private_dns_enabled = true
+    }
+    ecrdkr = {
+      service             = "ecr.dkr"
+      service_type        = "Interface"
+      private_dns_enabled = true
+    }
+    cloudwatchlogs = {
+      service             = "logs"
+      service_type        = "Interface"
+      private_dns_enabled = false
+    }
+    sqs = {
+      service             = "sqs"
+      service_type        = "Interface"
+      private_dns_enabled = false
+    }
+    sns = {
+      service             = "sns"
+      service_type        = "Interface"
+      private_dns_enabled = false
+    }
+  }
+
+  tags = local.tags_02
 }
